53d3a14907afc5a477c2cc9cdf808b94f1a9e69c
Jamie <iamielai2000@gmail.com>
2020-08-11 19:25:25 +0800

    Initial commit

---
+++ app/src/main/java/musicstream/tp/edu/finaltry/ScreenActivity.java
@@ -0,0 +1,12 @@
+package musicstream.tp.edu.finaltry;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+public class ScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen);
+    }
+}
